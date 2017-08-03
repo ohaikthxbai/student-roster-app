56582bba24f6e93a2feb37311d376c321c8c843f
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-08-02 21:11:03 -0500

    body parser stuff

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
 
 var PORT = 3000;
+
+// in order to take in POST data easily
+// process PUT and POST data; url query strings
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 var roster = [
     {
@@ -23,10 +33,16 @@ app.get("/", function(req, res) {
 app.get("/students", function(req, res) {
     res.json(roster);
 });
-
+// req = what's coming in
+// res = whtat's being outputted
 app.get('/students/:studentId', function(req, res) {
-    res.json(roster[req.params.studentId]);
+    res.json(roster[req.params.studentId].name);
 });
+
+app.post('/add', function(req, res){
+    
+});
+
 // get route that returns three stooges
 app.listen(PORT, function() {
     console.log(`Listening on PORT: ${PORT}`);
