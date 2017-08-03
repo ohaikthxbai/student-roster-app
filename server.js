5b63e814b52dae10099cd52fad24950b9c16ee0e
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-08-02 20:41:35 -0500

    GET method added

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var app = express();
 
 var PORT = 3000;
 
+
+app.get("/", function(req, res) {
+    res.send("Welcome to this page!");
+});
+
 app.listen(PORT, function() {
     console.log(`Listening on PORT: ${PORT}`);
 });
-
-// define some routes at some point
