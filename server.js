929c791a97695fe62c0a50433eb1abaacf85744e
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-08-02 20:55:24 -0500

    student id stuff

--- server.js
+++ server.js
@@ -3,11 +3,31 @@ var app = express();
 
 var PORT = 3000;
 
+var roster = [
+    {
+        name: 'Mark'
+    },
+    {
+        name: 'Suzune'
+    },
+    {
+        name: 'Saitama'
+    }
+];
 
+// define routes
 app.get("/", function(req, res) {
     res.send("Welcome to this page!");
 });
 
+app.get("/students", function(req, res) {
+    res.json(roster);
+});
+
+app.get('/students/:studentId', function(req, res) {
+    res.json(roster[req.params.studentId]);
+});
+// get route that returns three stooges
 app.listen(PORT, function() {
     console.log(`Listening on PORT: ${PORT}`);
 });
